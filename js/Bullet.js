aa7336c413ae49caad258be50a4aa9595b498703
Guypesto <gppressi@gmail.com>
2015-08-23 16:34:10 -0400

    Remove bullets when hitting enemy

--- js/Bullet.js
+++ js/Bullet.js
@@ -1,12 +1,15 @@
 Crafty.c("Bullet",{
 	init: function(){
-		this.requires("2D,DOM,Color").attr({			
+		this.requires("2D,DOM,Color,Collision").attr({			
 			yspeed: 10,
 			w: 2,
 			h: 5
 		}).color('#FFFFFF')
 		.bind("EnterFrame", function(){
 			this.y -= this.yspeed;
+		})
+		.onHit('Enemy', function() {
+			this.destroy();
 		});
 	}
 });
